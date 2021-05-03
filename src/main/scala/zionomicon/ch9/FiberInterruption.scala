61c942e2881be9e7b1f3efb83d7481a987060df4
Samuel SAYAG <samuel.sayag@clicktale.com>
2021-05-03 10:12:00 +0300

    advance chp9

--- src/main/scala/zionomicon/ch9/FiberInterruption.scala
+++ src/main/scala/zionomicon/ch9/FiberInterruption.scala
@@ -86,5 +86,60 @@ object BlockingInterrupt2 extends ZIOApp {
     blocking.effectBlockingInterrupt {
       Thread.sleep(10000L)
     }
+}
 
+object InterruptibleRegion1 extends ZIOApp {
+
+  def run(args: List[String]): URIO[ZEnv, ExitCode] =
+    program.tap(res => putStrLn(s"We got [$res]")).exitCode
+
+  def program = for {
+    ref   <- Ref.make(false)
+    fiber <- (ZIO.sleep(5.seconds) *> ref.set(true)).uninterruptible.fork
+    //fiber <- (ZIO.sleep(5.seconds) *> ref.set(true)).uninterruptible.delay(10.millis).fork
+    //fiber <- (ZIO.sleep(5.seconds) *> ref.set(true)).fork.uninterruptible
+    //fiber <- (ZIO.sleep(5.seconds).interruptible *> ref.set(true)).fork.uninterruptible
+    _     <- fiber.interrupt
+    value <- ref.get
+  } yield value
+}
+
+object InterruptibleRegion2 extends ZIOApp {
+
+  def run(args: List[String]): URIO[ZEnv, ExitCode] = program.exitCode
+
+  val effect1 = putStrLn("end of effect 1").delay(1.second)
+  val effect2 = putStrLn("end of effect 2").delay(2.second)
+  val effect3 = putStrLn("end of effect 3").delay(3.second)
+
+  val effectUninterruptible = (effect1 *> effect2 *> effect3).uninterruptible
+
+  val effectInterruptibleRegion = (effect1 *> effect2 *> effect3.interruptible).uninterruptible
+
+  val program = for {
+    //fiber <- effectUninterruptible.fork
+    fiber <- effectInterruptibleRegion.fork
+    _     <- fiber.interrupt
+  } yield ()
+
+}
+
+object WaitingForInterruption1 extends ZIOApp {
+
+  def run(args: List[String]): URIO[ZEnv, ExitCode] = program.exitCode
+
+  def effect[E, A](promise: Promise[E, A], a: A) =
+    putStrLn("Entering the effect") *> promise.succeed(a) *> ZIO.never
+
+  val program = for {
+    promise  <- Promise.make[Nothing, Unit]
+    finalizer = putStrLn("Finalizing effect...").delay(3.seconds) *> putStr("end of finalizer...")
+    // with this wait for the finalizer to finish which is normal
+    //fiber    <- effect(promise, ()).ensuring(finalizer).disconnect.fork
+    fiber    <- effect(promise, ()).ensuring(finalizer).fork
+    _        <- promise.await
+    //_        <- fiber.interrupt
+    _        <- fiber.interrupt.fork
+    _        <- putStrLn("Done interrupting...")
+  } yield ()
 }
